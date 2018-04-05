44574811fb3fc58ad5845ebb5b69bc83bf76a482
HoangNguyenQuang <hoangnguyenquang@vccorp.vn>
2018-04-05 09:42:26 +0700

    - Update Installation - Update automatic install & update

---
+++ Path-Of-Exile-TradeItemCopy.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         PathOfExile Trade Offical Item
+// @namespace    http://tampermonkey.net/
+// @description  PathOfExile Trade to Path of Building
+// @source       https://github.com/fikal/PoeTradeItemCopy
+// @author       HoangStorm
+// @match        https://www.pathofexile.com/trade/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.6.0/clipboard.min.js
+// @version      0.1.0
+// @grant        none
+// @downloadURL  https://github.com/hoangstorm/Path-Of-Exile-TradeItemCopy/raw/master/Path-Of-Exile-TradeItemCopy.user.js
+// @updateURL    https://github.com/hoangstorm/Path-Of-Exile-TradeItemCopy/raw/master/Path-Of-Exile-TradeItemCopy.meta.js
+// ==/UserScript==
